4f079cac2235940ae624f91cf2a2421cd8e64e50
Mohotazim-Bosunia <zimhossen0@gmail.com>
2025-07-08 23:05:39 +0600

    second commit

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,13 +1,28 @@
-
+import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
-    const {title,cover}=blog;
+    const {title,cover,author_img,reading_time,author,posted_date}=blog;
     return (
         <div>
-            <h1>Title:{title}</h1>
-            <img src={cover} alt="" />
+           
+            <img src={cover} alt={`Cover picture of the title  ${title}`} />
+            <div className='md:flex justify-between items-center'>
+               <div className='md:flex items-center'>
+                 <img src={author_img} alt=""  className='w-13 h-13 rounded-full'/>
+                 <div className='ml-6'>
+                    <h1 className='text-3xl font-bold'>{author}</h1>
+                 <h1 className='text-2xl'>{posted_date}</h1>
+                 </div>
+               </div>
+                <span>{reading_time }min read</span>
+            </div>
+             <h1 className='texr-4xl'>Title:{title}</h1>
         </div>
     );
 };
+
+Blog.propTypes={
+    blog:PropTypes.object.isRequired
+}
 
 export default Blog;
